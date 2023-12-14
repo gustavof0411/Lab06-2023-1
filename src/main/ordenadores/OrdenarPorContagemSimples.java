044cdaf60b19e3e1e1ec599953700407288f0a58
gustavof0411 <136523482+gustavof0411@users.noreply.github.com>
2023-12-14 20:37:02 +0000

    Implementados Particiona e ContagemSimples

--- src/main/ordenadores/OrdenarPorContagemSimples.java
+++ src/main/ordenadores/OrdenarPorContagemSimples.java
@@ -1,11 +1,41 @@
 package ordenadores;
 
-public class OrdenarPorContagemSimples implements Ordenador{
+public class OrdenarPorContagemSimples implements Ordenador {
 
     @Override
     public int[] ordene(int[] array) {
+        int maior = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maior) {
+                maior = array[i];
+            }
+        }
+        boolean[] arrayBool = new boolean[maior+1];
 
-        return array;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                arrayBool[0] = true;
+            } else {
+            arrayBool[array[i]] = true;
+        }
     }
-    
+        int quantidadeVerdadeiros = 0;
+        for (int i = 0; i < arrayBool.length; i++) {
+            if (arrayBool[i] == true) {
+                quantidadeVerdadeiros++;
+            }
+        }
+        int[] arrayOrdenado = new int[quantidadeVerdadeiros];
+        int indiceArrayOrdenado = 0;
+        for (int i = 0; i < arrayBool.length; i++) {
+            if (arrayBool[i] == true) {
+                arrayOrdenado[indiceArrayOrdenado] = i;
+            }
+            indiceArrayOrdenado++;
+        }
+        
+
+        return arrayOrdenado;
+    }
+
 }
--- src/main/ordenadores/OrdenarQuick.java
+++ src/main/ordenadores/OrdenarQuick.java
@@ -1,14 +1,36 @@
 package ordenadores;
-public class OrdenarQuick implements Ordenador{
+
+public class OrdenarQuick implements Ordenador {
 
     @Override
     public int[] ordene(int[] array) {
-
         return array;
     }
 
-    public int quickParticio(int[] array, int inicio, int fim){
-        return 0;
+    public int quickParticio(int[] array, int inicio, int fim) {
+        int indicePivo = inicio;
+        if (inicio <= fim) {
+            int pivo = array[inicio];
+            int indiceSalvo = inicio;
+            for (int i = inicio; i <= fim; i++) {
+                if (array[i] < pivo) {
+                    indiceSalvo++;
+                    int elementoTroca = array[indiceSalvo];
+                    array[indiceSalvo] = array[i];
+                    array[i] = elementoTroca;
+                }
+            }
+            for (int i = inicio; i <= fim; i++) {
+                if (pivo > array[i]) {
+                    int elementoTroca = array[i];
+                    array[i] = pivo;
+                    array[indicePivo] = elementoTroca;
+                    indicePivo++;
+                } else if (pivo < array[i] && pivo != array[i]) {
+                    break;
+                }
+            }
+        }
+        return indicePivo;
     }
-    
 }
